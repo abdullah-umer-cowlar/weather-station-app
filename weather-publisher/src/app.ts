711f855e3566007f50295a453f907e5885646c2c
abdullah-umer-cowlar <abdullah.umer.cowlar@gmail.com>
2024-07-03 16:11:17 +0500

    feat: random data logging every 2 seconds

--- weather-publisher/src/app.ts
+++ weather-publisher/src/app.ts
@@ -1,6 +1,7 @@
 import mqtt from "mqtt";
 import dotenv from "dotenv";
 import envConfig from "./config/envConfig";
+import { getRandomFloat } from "./utils";
 
 dotenv.config();
 const client = mqtt.connect(
@@ -9,24 +10,28 @@ const client = mqtt.connect(
 
 client.on("connect", () => {
   console.log("Weather publisher successfully connected to mqtt broker.");
+  console.log("Starting data publishing...");
   initSimulator();
 });
 
-const data = {
-  weather_data: {
-    temperature: 15,
-    humidity: 25,
-  },
-};
-
 const initSimulator = () => {
   setInterval(() => {
-    client.publish("test", JSON.stringify(data));
-  }, 1000);
+    const randomData = {
+      weather_data: {
+        temperature: getRandomFloat(10, 40),
+        humidity: getRandomFloat(15, 25),
+      },
+    };
+    client.publish(envConfig.TOPIC_NAME, JSON.stringify(randomData), (err) => {
+      if (err) {
+        console.error("Error publishing data:", err);
+      }
+    });
+  }, 2000);
 };
 
 client.on("message", (topic, message) => {
-  // message is Buffer
-  console.log("recv topic: ", topic);
-  console.log("recv message: ", message.toString());
+  console.log("New message received.", topic);
+  console.log("Topic: ", topic);
+  console.log("Content: ", message.toString());
 });
--- weather-publisher/src/config/envConfig.ts
+++ weather-publisher/src/config/envConfig.ts
@@ -4,6 +4,7 @@ dotenv.config();
 const envConfig = {
   BROKER_HOST: process.env.BROKER_HOST,
   BROKER_PORT: process.env.BROKER_PORT,
+  TOPIC_NAME: process.env.TOPIC_NAME,
 };
 
 export default envConfig;
