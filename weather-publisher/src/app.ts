f65f07299438a539c5c916281843528c2b1decbe
abdullah-umer-cowlar <abdullah.umer.cowlar@gmail.com>
2024-07-03 16:30:34 +0500

    feat: add control topic and handling for stop/starts during runtime

--- weather-publisher/src/app.ts
+++ weather-publisher/src/app.ts
@@ -8,14 +8,23 @@ const client = mqtt.connect(
   `mqtt://${envConfig.BROKER_HOST}:${envConfig.BROKER_PORT}`
 );
 
+let publishTimer = null;
+
 client.on("connect", () => {
   console.log("Weather publisher successfully connected to mqtt broker.");
+  console.log("Subscribing to control topic...");
+  client.subscribe("wsa/control", (err) => {
+    if (err) {
+      console.error("Error subscribing to control topic:", err);
+    }
+  });
   console.log("Starting data publishing...");
   initSimulator();
 });
 
+// fine being QoS 1 i think
 const initSimulator = () => {
-  setInterval(() => {
+  publishTimer = setInterval(() => {
     const randomData = {
       weather_data: {
         temperature: getRandomFloat(10, 40),
@@ -30,8 +39,26 @@ const initSimulator = () => {
   }, 2000);
 };
 
+// this needs to be QoS 2
 client.on("message", (topic, message) => {
-  console.log("New message received.", topic);
+  console.log("New message received.");
   console.log("Topic: ", topic);
   console.log("Content: ", message.toString());
+  if (topic === "wsa/control") {
+    try {
+      const data = JSON.parse(message.toString());
+      if (!data.command) {
+        throw new Error("Invalid data, command not found.");
+      }
+      if (data.command === "start") {
+        initSimulator();
+      } else if (data.command === "stop") {
+        clearInterval(publishTimer);
+      } else {
+        throw new Error("Invalid command.");
+      }
+    } catch (err) {
+      console.error("Error while processing received message:", err);
+    }
+  }
 });
